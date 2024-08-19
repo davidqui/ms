4d26bf0fea951fcd96cf355fb006f58497e883d2
D.Q.R <davidquijanoramos@gmail.com>
2024-08-19 10:16:39 -0500

    Introducción a spring cloud streams
    1) Se agrega el nuevo proyecto Report-listener

---
+++ repor-listener/src/main/java/com/quijano/repor_listener/ReporListenerApplication.java
@@ -0,0 +1,13 @@
+package com.quijano.repor_listener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporListenerApplication.class, args);
+	}
+
+}
---
+++ repor-listener/src/test/java/com/quijano/repor_listener/ReporListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quijano.repor_listener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
