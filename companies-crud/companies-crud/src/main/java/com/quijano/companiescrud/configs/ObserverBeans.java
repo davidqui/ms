d2a2226431776ad31059ed1879462e507b16f20f
D.Q.R <davidquijanoramos@gmail.com>
2024-08-22 20:41:10 -0500

    Configurando métricas y trazabilidad parte 4

--- companies-crud/companies-crud/src/main/java/com/quijano/companiescrud/configs/ObserverBeans.java
+++ companies-crud/companies-crud/src/main/java/com/quijano/companiescrud/configs/ObserverBeans.java
@@ -1,9 +1,12 @@
 package com.quijano.companiescrud.configs;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
-import io.micrometer.tracing.annotation.MethodInvocationProcessor;
-import io.micrometer.tracing.annotation.SpanAspect;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.annotation.*;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.context.propagation.ContextPropagators;
@@ -15,6 +18,8 @@ import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.semconv.ResourceAttributes;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -65,7 +70,32 @@ public class ObserverBeans {
                 .builder(otlpLogRecord).build();
     }
 
+    @Bean
     public SpanAspect spanAspect(MethodInvocationProcessor processor){
         return new SpanAspect(processor);
+    }
+
+    @Bean
+    public MethodInvocationProcessor methodInvocationProcessor(NewSpanParser spanParser,
+                                     Tracer tracer,
+                                     BeanFactory beanFactory){
+        return new ImperativeMethodInvocationProcessor(
+                spanParser,
+                tracer,
+                beanFactory::getBean,
+                beanFactory::getBean
+
+        );
+    }
+
+    @Bean
+    public NewSpanParser newSpanParser(){
+        return new DefaultNewSpanParser();
+
+    }
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry){
+        return new TimedAspect(registry);
     }
 }
