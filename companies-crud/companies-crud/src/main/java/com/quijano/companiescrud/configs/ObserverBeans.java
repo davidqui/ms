977c8e6afcf1bd2692f5c58330afbf2e5d41e081
D.Q.R <davidquijanoramos@gmail.com>
2024-08-22 20:26:50 -0500

    Configurando métricas y trazabilidad parte 3

--- companies-crud/companies-crud/src/main/java/com/quijano/companiescrud/configs/ObserverBeans.java
+++ companies-crud/companies-crud/src/main/java/com/quijano/companiescrud/configs/ObserverBeans.java
@@ -2,11 +2,18 @@ package com.quijano.companiescrud.configs;
 
 import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.observation.aop.ObservedAspect;
+import io.micrometer.tracing.annotation.MethodInvocationProcessor;
+import io.micrometer.tracing.annotation.SpanAspect;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.http.logs.OtlpHttpLogRecordExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.logs.LogRecordProcessor;
 import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
 import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.semconv.ResourceAttributes;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
@@ -33,5 +40,32 @@ public class ObserverBeans {
                 .setResource(Resource.getDefault().merge(springResource));
         processor.orderedStream().forEach(builder::addLogRecordProcessor);
         return builder.build();
+    }
+
+    @Bean
+    public OpenTelemetry openTelemetry(SdkLoggerProvider loggerProvider,
+                                       SdkTracerProvider tracerProvider,
+                                       ContextPropagators contextPropagators){
+        return OpenTelemetrySdk
+                .builder()
+                    .setLoggerProvider(loggerProvider)
+                    .setTracerProvider(tracerProvider)
+                    .setPropagators(contextPropagators)
+                .build();
+    }
+
+    @Bean
+    public LogRecordProcessor logRecordProcessor(){
+        var otlpLogRecord = OtlpHttpLogRecordExporter
+                .builder()
+                .setEndpoint("http://localhost:4317")
+                .build();
+
+        return BatchLogRecordProcessor
+                .builder(otlpLogRecord).build();
+    }
+
+    public SpanAspect spanAspect(MethodInvocationProcessor processor){
+        return new SpanAspect(processor);
     }
 }
