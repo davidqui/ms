896971cf153d275f16586fa9fde4046c6153c16d
D.Q.R <davidquijanoramos@gmail.com>
2024-08-26 20:45:25 -0500

    Añadiendo filtro a nuestras rutas

--- gateway/src/main/java/com/quijano/gateway/dtos/TokenDto.java
+++ gateway/src/main/java/com/quijano/gateway/dtos/TokenDto.java
@@ -1,23 +1,14 @@
 package com.quijano.gateway.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TokenDto {
-
-    public TokenDto(){
-
-    }
-
-    public TokenDto(String accessToken) {
-        this.accessToken = accessToken;
-    }
 
     private String accessToken;
 
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
 }
